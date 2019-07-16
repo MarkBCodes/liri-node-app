524195ec857adb7173e37a0d43319f4f6095a1be
Mark B <mark.bullard42@gmail.com>
2019-07-16 16:36:11 -0400

    added switch for user input

--- liri.js
+++ liri.js
@@ -10,3 +10,27 @@ var request = require("request");
 //input capture
 var userChoices = process.argv[2];
 var paramInput = process.argv[3];
+
+inputOfUser(userChoices, paramInput);
+
+//Switch for commands
+function inputOfUser(userChoices, paramInput) {
+  switch (userChoices) {
+    case "concert-this":
+      displayConcertInfo(paramInput);
+      break;
+    case "spotify-this-song":
+      displaySongInfo(paramInput);
+      break;
+    case "movie-this":
+      displayMovieInfo(paramInput);
+      break;
+    case "do-what-it-says":
+      sumInfo();
+      break;
+    default:
+      console.log(
+        "Invalid! Type in any of the following: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says"
+      );
+  }
+}
