a68c0ad59f63c039a03d2b98892fed68ac9475a6
Mark B <mark.bullard42@gmail.com>
2019-07-16 14:14:31 -0400

    set up initial variables that will help me pull from files and APIs

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
 
-var keys = require("../liri-node-app/keys.js");
+var keys = require("./keys.js");
+
+var spotify = new spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var request = require("request");
+
+//input capture
+var userChoices = process.argv[2];
+var paramInput = process.argv[3];
